b5544fa4290347f139e81f43e51e118ebd0275c2
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-05-06 15:59:50 +0200

    added navbar and some content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,1 +1,11 @@
 import styles from "@/app/about/about.module.css";
+import Navbar from "@/components/Navbar/Navbar";
+
+export default function About() {
+  return (
+    <main>
+      <Navbar />
+      <h1>About</h1>
+    </main>
+  );
+}
