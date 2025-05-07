20b8f079548841dfda23a1e6527715f4bff5e085
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-05-07 13:22:44 +0200

    added curse of calculus page

---
+++ src/app/curse-of-calculus/page.tsx
@@ -0,0 +1,11 @@
+import styles from "@/app/curse-of-calculus/curse-of-calculus.module.css";
+import Navbar from "@/components/Navbar/Navbar";
+
+export default function CurseOfCalculus() {
+  return (
+    <main>
+      <Navbar />
+      <h1>Curse of Calculus</h1>
+    </main>
+  );
+}
