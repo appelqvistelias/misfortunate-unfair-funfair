82c36a22aa73380033e89dc62b9bd5ce4a577aa2
Sandra <sandra0924@protonmail.com>
2025-05-08 17:35:34 +0200

    added comment with text to remember to implement further into development

--- src/app/madame-misfortune/components/Card.tsx
+++ src/app/madame-misfortune/components/Card.tsx
@@ -22,3 +22,16 @@ export default function Card({ card, isSelected, isFaded, onClick }: CardProps) 
     </div>
   );
 }
+
+
+/**
+ * Three signs of doom from hand you've played—
+Now hear what wicked fate's been laid...
+ */
+
+/**
+ * Your fate is sealed, your doom’s in sight,
+Hope you enjoyed this little fright.
+No need to scream, no use to pray—
+Misfortune loves to lead the way.
+*/
