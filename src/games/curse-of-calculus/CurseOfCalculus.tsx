16af6a1f266c337011df67da9bbaee1ab7547d17
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-05-09 17:02:34 +0200

    prepared game with react hooks

--- src/games/curse-of-calculus/CurseOfCalculus.tsx
+++ src/games/curse-of-calculus/CurseOfCalculus.tsx
@@ -1,3 +1,24 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import mathPairs from "@/data/mathPairs.json";
+import { generateCards } from "@/games/curse-of-calculus/utils";
+import { CurseCard, CursePair } from "@/games/curse-of-calculus/types";
+
+const card_pairs = 9;
+
+export default function CurseOfCalculus() {
+  const [cards, setCards] = useState<CurseCard[]>([]);
+  const [selectedCards, setSelectedCards] = useState<CurseCard[]>([]);
+  const [matchedPairs, setMatchedPairs] = useState<number[]>([]);
+
+  useEffect(() => {
+    const pairs = mathPairs as CursePair[];
+    const newCards = generateCards(pairs, card_pairs);
+    setCards(newCards);
+  });
+}
+
 // To do:
 // Import pairs from .json file
 // Use utils function to generate random pairs
