85fd08dc6e4c5e7977ff97f4796cf11c2d35e6ad
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-05-12 15:25:09 +0200

    added game to page

--- src/app/curse-of-calculus/page.tsx
+++ src/app/curse-of-calculus/page.tsx
@@ -1,11 +1,13 @@
 import styles from "@/app/curse-of-calculus/curse-of-calculus.module.css";
 import Navbar from "@/components/Navbar/Navbar";
+import CurseOfCalculus from "@/games/curse-of-calculus/CurseOfCalculus";
 
-export default function CurseOfCalculus() {
+export default function CurseOfCalculusPage() {
   return (
     <main>
       <Navbar />
       <h1>Curse of Calculus</h1>
+      <CurseOfCalculus />
     </main>
   );
 }
