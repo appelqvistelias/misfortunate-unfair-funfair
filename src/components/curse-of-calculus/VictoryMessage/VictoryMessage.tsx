7e7a610ff0292eb1afc3653218e742c034bcb1d4
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-05-12 15:13:22 +0200

    added victory message

---
+++ src/components/curse-of-calculus/VictoryMessage/VictoryMessage.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "@/components/curse-of-calculus/VictoryMessage/VictoryMessage.module.css";
+
+export default function VictoryMessage() {
+  return <div className={styles.victoryMessage}>🎉 You made it!</div>;
+}
