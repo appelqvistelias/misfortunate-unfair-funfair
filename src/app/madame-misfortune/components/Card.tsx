eed5ab3243eb3a8f64507aabef4d9f5fc44e4182
Sandra <sandra0924@protonmail.com>
2025-05-12 11:33:23 +0200

    altered code style with prettier

--- src/app/madame-misfortune/components/Card.tsx
+++ src/app/madame-misfortune/components/Card.tsx
@@ -1,49 +1,48 @@
-'use client';
+"use client";
 
-import styles from './card.module.css';
-import { TarotCard } from '../types';
+import styles from "./card.module.css";
+import { TarotCard } from "../types";
 
 type CardProps = {
-    card: TarotCard;
-    isSelected: boolean;
-    isFaded?: boolean;
-    isRevealed?: boolean;
-    onClick?: (id: number) => void;
-};  
+  card: TarotCard;
+  isSelected: boolean;
+  isFaded?: boolean;
+  isRevealed?: boolean;
+  onClick?: (id: number) => void;
+};
 
 export default function Card({
-    card,
-    isSelected,
-    isFaded = false,
-    isRevealed = false,
-    onClick
-  }: CardProps) {
-    const handleClick = () => {
-      if (onClick) onClick(card.id);
-    };
-  
-    return (
-      <div
-        className={`
+  card,
+  isSelected,
+  isFaded = false,
+  isRevealed = false,
+  onClick,
+}: CardProps) {
+  const handleClick = () => {
+    if (onClick) onClick(card.id);
+  };
+
+  return (
+    <div
+      className={`
             ${styles.card} 
-            ${isFaded ? styles.faded : ''} 
-            ${isSelected ? styles.selected : ''}
-            ${isRevealed ? styles.revealed : ''}
+            ${isFaded ? styles.faded : ""} 
+            ${isSelected ? styles.selected : ""}
+            ${isRevealed ? styles.revealed : ""}
         `}
-        onClick={handleClick}
-      >
-        {isRevealed && (
-          <>
-            <img
-                src={isRevealed ? card.imageUrl : '/images/tarot/card-back.png'}
-                alt={card.name}
-                className={styles.cardImage}
-            />
-            <h3>{card.name}</h3>
-            <p>{card.text}</p>
-          </>
-        )}
-      </div>
-    );
-  }
-  
+      onClick={handleClick}
+    >
+      {isRevealed && (
+        <>
+          <img
+            src={isRevealed ? card.imageUrl : "/images/tarot/card-back.png"}
+            alt={card.name}
+            className={styles.cardImage}
+          />
+          <h3>{card.name}</h3>
+          <p>{card.text}</p>
+        </>
+      )}
+    </div>
+  );
+}
