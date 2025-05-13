eafa03807050307f49ee70c3ee619e7e921ba335
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-05-13 12:12:57 +0200

    started styling game page

--- src/app/curse-of-calculus/page.tsx
+++ src/app/curse-of-calculus/page.tsx
@@ -1,13 +1,29 @@
+import { Offside } from "next/font/google";
+const offside = Offside({ subsets: ["latin"], weight: "400" });
+
+import { Josefin_Sans } from "next/font/google";
+const josefin_sans = Josefin_Sans({ subsets: ["latin"], weight: "400" });
+
 import styles from "@/app/curse-of-calculus/curse-of-calculus.module.css";
 import Navbar from "@/components/Navbar/Navbar";
 import CurseOfCalculus from "@/games/curse-of-calculus/CurseOfCalculus";
 
 export default function CurseOfCalculusPage() {
   return (
-    <main>
+    <>
       <Navbar />
-      <h1>Curse of Calculus</h1>
-      <CurseOfCalculus />
-    </main>
+      <main className={styles.mainWithBackground}>
+        <h1 className={`${offside.className} ${styles.title}`}>
+          Curse of Calculus
+        </h1>
+        <p className={`${josefin_sans.className} ${styles.paragraph}`}>
+          Remember asking in Math class: “Why do we learn this?”
+        </p>
+        <p className={`${josefin_sans.className} ${styles.paragraph}`}>
+          Welcome to why.
+        </p>
+        <CurseOfCalculus />
+      </main>
+    </>
   );
 }
