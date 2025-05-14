a00cac1ba4cb1cdd3fc0312f237a1a842beb144e
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-05-14 13:44:42 +0200

    cleaned up xml and added some aria labels

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,9 +8,11 @@ export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.wrapper}>
-        <p className={styles.logo}>Unfair Funfair</p>
+    <header>
+      <nav className={styles.navbar}>
+        <Link className={styles.logo} href="/" aria-label="Go to home page">
+          Unfair Funfair
+        </Link>
 
         <button
           className={`${styles.hamburger} ${menuOpen ? styles.open : ""}`}
@@ -21,30 +23,46 @@ export default function Navbar() {
           <span />
           <span />
         </button>
-      </div>
 
-      <ul className={`${styles.navList} ${menuOpen ? styles.navOpen : ""}`}>
-        <li>
-          <Link href="/" onClick={() => setMenuOpen(false)}>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href="/about" onClick={() => setMenuOpen(false)}>
-            About
-          </Link>
-        </li>
-        <li>
-          <Link href="/curse-of-calculus" onClick={() => setMenuOpen(false)}>
-            Curse Of Calculus
-          </Link>
-        </li>
-        <li>
-          <Link href="/madame-misfortune" onClick={() => setMenuOpen(false)}>
-            Madame Misfortune
-          </Link>
-        </li>
-      </ul>
-    </nav>
+        <ul className={`${styles.navList} ${menuOpen ? styles.navOpen : ""}`}>
+          <li>
+            <Link
+              href="/"
+              aria-label="Go to home page"
+              onClick={() => setMenuOpen(false)}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about"
+              aria-label="Go to about page"
+              onClick={() => setMenuOpen(false)}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/curse-of-calculus"
+              aria-label="Go to Curse of Calculus game page"
+              onClick={() => setMenuOpen(false)}
+            >
+              Curse Of Calculus
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/madame-misfortune"
+              aria-label="Go to Madame Misfortune game page"
+              onClick={() => setMenuOpen(false)}
+            >
+              Madame Misfortune
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
