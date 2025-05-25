2b424da3bfd3b71c4a61f0534e1bd3b25817df10
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-05-25 13:57:17 +0200

    added popup window for rules

--- src/app/curse-of-calculus/page.tsx
+++ src/app/curse-of-calculus/page.tsx
@@ -1,14 +1,21 @@
+"use client";
+
 import { Offside } from "next/font/google";
 const offside = Offside({ subsets: ["latin"], weight: "400" });
 
 import { Josefin_Sans } from "next/font/google";
 const josefin_sans = Josefin_Sans({ subsets: ["latin"], weight: "400" });
 
+import { useState } from "react";
 import styles from "@/app/curse-of-calculus/curse-of-calculus.module.css";
 import Navbar from "@/components/Navbar/Navbar";
 import CurseOfCalculus from "@/games/curse-of-calculus/CurseOfCalculus";
+import Button from "@/components/Button/Button";
+import PopUp from "@/components/PopUp/PopUp";
 
 export default function CurseOfCalculusPage() {
+  const [isPopUpOpen, setIsPopUpOpen] = useState(false);
+
   return (
     <>
       <Navbar />
@@ -30,6 +37,17 @@ export default function CurseOfCalculusPage() {
               <p className={`${josefin_sans.className} ${styles.paragraph}`}>
                 Welcome to why.
               </p>
+              <Button text="Rules" onClick={() => setIsPopUpOpen(true)} />
+
+              <PopUp
+                isOpen={isPopUpOpen}
+                onClose={() => setIsPopUpOpen(false)}
+                title="Game Rules"
+              >
+                <p className={josefin_sans.className}>
+                  Here are the rules of the game...
+                </p>
+              </PopUp>
             </div>
           </div>
           <CurseOfCalculus />
